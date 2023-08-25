c07bf2aa6fa41f90df62636146778f4388800da5
MithkaReece <reece.waite1@gmail.com>
2023-08-25 22:01:06 +0100

    Walking animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,30 +6,35 @@ public class Player : MonoBehaviour
 {
   [SerializeField] private float movementSpeed;
 
-  // Update is called once per frame
-  void Update()
-  {
-    Vector3 movementVector = new Vector3(0, 0, 0);
-    if (Input.GetKey(KeyCode.W)) {
-      movementVector.z = 1;
-    }
-    if (Input.GetKey(KeyCode.A))
-    {
-      movementVector.x = -1;
-    }
-    if (Input.GetKey(KeyCode.S))
-    {
-      movementVector.z = -1;
-    }
-    if (Input.GetKey(KeyCode.D))
-    {
-      movementVector.x = 1;
-    }
-    movementVector.Normalize();
+    private bool isWalking;
 
-    transform.position += movementVector * movementSpeed * Time.deltaTime;
+    // Update is called once per frame
+    void Update(){
+        Vector3 movementVector = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W)) {
+            movementVector.z = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movementVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movementVector.z = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementVector.x = 1;
+        }
+        movementVector.Normalize();
 
-    float rotateSpeed = 10f;
-    transform.forward = Vector3.Slerp(transform.forward, movementVector, Time.deltaTime * rotateSpeed);
-  }
+        transform.position += movementVector * movementSpeed * Time.deltaTime;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, movementVector, Time.deltaTime * rotateSpeed);
+
+        isWalking = movementVector != Vector3.zero;
+    }
+
+    public bool IsWalking() { return isWalking; }
 }
