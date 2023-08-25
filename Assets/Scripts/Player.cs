df6b7478ce9958ae83df89ca487e74faf1c13cee
MithkaReece <reece.waite1@gmail.com>
2023-08-25 21:47:56 +0100

    Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  [SerializeField] private float movementSpeed;
+
+  // Update is called once per frame
+  void Update()
+  {
+    Vector3 movementVector = new Vector3(0, 0, 0);
+    if (Input.GetKey(KeyCode.W)) {
+      movementVector.z = 1;
+    }
+    if (Input.GetKey(KeyCode.A))
+    {
+      movementVector.x = -1;
+    }
+    if (Input.GetKey(KeyCode.S))
+    {
+      movementVector.z = -1;
+    }
+    if (Input.GetKey(KeyCode.D))
+    {
+      movementVector.x = 1;
+    }
+    movementVector.Normalize();
+
+    transform.position += movementVector * movementSpeed * Time.deltaTime;
+
+    float rotateSpeed = 10f;
+    transform.forward = Vector3.Slerp(transform.forward, movementVector, Time.deltaTime * rotateSpeed);
+  }
+}
